ae5fabbc476e11625398519cef8bfbb2f5f258c3
michaelschafhauser <92251466+michaelschafhauser@users.noreply.github.com>
2021-12-07 21:41:23 +0000

    Searchbar updates

--- searchbar.py
+++ searchbar.py
@@ -3,6 +3,18 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+st.sidebar.markdown(f"""
+    # Football MVP
+    """)
+
+FONT_SIZE_CSS = f"""
+<style>
+h1 {{
+    font-size: 36px !important;
+}}
+</style>
+"""
+st.write(FONT_SIZE_CSS, unsafe_allow_html=True)
 
 players = pd.read_csv("players.csv")
 players = players.drop_duplicates()
