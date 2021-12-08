b1f686869d98f3d99b60c4c571336588827ff67d
Ricardo Maia <ricardocm77@hotmail.com>
2021-12-08 16:27:38 +0000

    New version of datafram to the FrontEND representing player skills

--- stats.py
+++ stats.py
@@ -4,15 +4,20 @@ import pandas as pd
 import requests
 from requests.structures import CaseInsensitiveDict
 import json
+#from dotenv import load_dotenv
+import os
+from bs4 import BeautifulSoup
+import plotly.express as px
 
+#load_dotenv()
 headers = CaseInsensitiveDict()
 headers["accept"] = "application/json"
-headers["X-AUTH-TOKEN"] = "6ee5d299-299c-480c-ba52-514607532d6a"
+#headers["X-AUTH-TOKEN"] = os.getenv("AUTH-TOKEN")
+headers["X-AUTH-TOKEN"]="6ee5d299-299c-480c-ba52-514607532d6a"
 headers["Content-Type"] = "application/json"
 
-
 # get the player name from the user's input
-player = st.text_input(label='input player name')
+player = st.text_input(label='Insert Player Name')
 
 # prediction URL ================================================
 URL1 = 'https://market-value-api-om3gdzslta-ew.a.run.app/predict'
@@ -23,25 +28,43 @@ params_url1 = {
 # get the data from our prediction api
 prediction_data = requests.get(URL1, params=params_url1)
 data = prediction_data.json()
-#data
+"""
+# 🎉 Player estimated Value 🎉
+"""
 
-# futdb URL =====================================================
+st.metric("Predicted value in millions of euros", data['prediction'])
+
+# futdb name search URL =========================================
 URL2 = "https://futdb.app/api/players/search"
 
 params_url2 = {'name': player}
-data2 = json.dumps(params_url2)
-resp = requests.post(URL2, headers=headers, data=data2).json()
+api_call = json.dumps(params_url2)
+resp = requests.post(URL2, headers=headers, data=api_call).json()
+searched_player = resp['items'][0]
+#searched_player
+
+list_of_stats = ['name', 'age', 'height', 'weight']
+stats_dictionary = {}
+for stat in list_of_stats:
+    stats_dictionary[stat] = str(searched_player[stat])
+
+# futdb image search URL =========================================
+URL3 = f"https://futdb.app/api/players/{searched_player['id']}/image"
+player_photo = requests.get(URL3)
+player_photo.url  #this line will provide a link to the photo
 
 player = resp['items'][0]
 #player
-player['name']
-player['age']
-player['height']
-player['weight']
+
+#{player['name']}
+st.metric("Player name",player['name'])
+#player['age']
+#player['height']
+#player['weight']
 league_ref=player["league"]
 nation_ref=player["nation"]
 club_ref=player["club"]
-data["prediction"]
+#data["prediction"]
 
 player_geo_information={}
 
@@ -49,12 +72,12 @@ player_geo_information={}
 from dict_country_club import country_dict
 for i in range(len(country_dict)):
     if nation_ref == country_dict[i]["id"]:
-        player_geo_information["Nationality"]=(country_dict[i]["name"])
+        player_geo_information["Nationality"] = (country_dict[i]["name"])
 
 # Leagues =====================================================
-URL3 = "https://futdb.app/api/leagues/"
-params_url3 = {'id': league_ref}
-league_resp = requests.get(URL3, headers=headers, params=params_url3).json()
+URL4 = "https://futdb.app/api/leagues/"
+params_url4 = {'id': league_ref}
+league_resp = requests.get(URL4, headers=headers, params=params_url4).json()
 league_name=league_resp['items']
 for i in range(len(league_name)):
     if league_ref==league_name[i]["id"]:
@@ -66,8 +89,12 @@ for i in range(len(club_dict)):
     if league_ref == club_dict[i]["league"] and club_ref == club_dict[i]["id"]:
         player_geo_information["Club"] = (club_dict[i]["name"])
 
-df=pd.DataFrame.from_dict(player_geo_information,orient="index")
-df
+
+df_geographic_loc = pd.DataFrame.from_dict(player_geo_information,
+                                           orient="index",
+                                           columns=['Player Stats'])
+
+
 
 # Club Images ================================================
 #URL6 = "https://futdb.app/api/clubs/{club_ref}}/image"
@@ -76,12 +103,50 @@ df
 
 
 @st.cache
+def get_player_stats():
+    return pd.DataFrame.from_dict(stats_dictionary,
+                                  orient='index',
+                                  columns=['Player Stats'])
+
+
+stats_df = get_player_stats()
+#st.table(stats_df)
+
+frames = [stats_df, df_geographic_loc]
+df_concat = pd.concat(frames, sort=False)
+df_concat=df_concat.drop(labels=['name'], axis=0)
+df_concat = df_concat.rename({
+    "age": "Age",
+    "height": "Height (cm)",
+    "weight": "Weight (kg)"
+})
+df_concat
+
+@st.cache
 def get_dataframe_data():
     return pd.DataFrame.from_dict(data['features'])
 
 
+# RADAR CHART =======================================================
+def generate_radar_chart():
+    r = []
+    theta = []
+    for key, value in data['features'].items():
+        r.append(value[0])
+        theta.append(key)
+
+    df = pd.DataFrame(dict(r=r, theta=theta))
+    fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+    return fig
+
+
+'''
+# Player Atributes
+'''
+
 df = get_dataframe_data()
 hdf = df.assign(hack='').set_index('hack')
-
-st.write("Player's attributes")
 st.table(hdf)
+
+radar = generate_radar_chart()
+st.write(radar)
