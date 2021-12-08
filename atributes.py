ae8ac7b833593f96053ed16de94f0448dab0b23e
joao robarts <jpsrobarts@gmail.com>
2021-12-08 10:20:16 +0000

    working on displaying player photo

--- atributes.py
+++ atributes.py
@@ -25,20 +25,38 @@ prediction_data = requests.get(URL1, params=params_url1)
 data = prediction_data.json()
 # data
 
-# futdb URL =====================================================
+# futdb name search URL =====================================================
 URL2 = "https://futdb.app/api/players/search"
 
 params_url2 = {'name' : player}
 api_call = json.dumps(params_url2)
 resp = requests.post(URL2, headers=headers, data=api_call).json()
 searched_player = resp['items'][0]
+#searched_player
 
 list_of_stats = ['name', 'age', 'height', 'weight']
 stats_dictionary = {}
 for stat in list_of_stats:
     stats_dictionary[stat] = str(searched_player[stat])
-stats_dictionary['predicted_value'] = data['prediction']
-stats_dictionary
+stats_dictionary['predicted_value_millions'] = data['prediction']
+
+# futdb image search URL ===================================================
+URL3 = f"https://futdb.app/api/players/{searched_player['id']}/image"
+player_photo = requests.get(URL3, headers=headers)
+player_photo.url #this line will provide a link to the photo
+
+
+a = f"""Some text
+
+![Cool Image]({player_photo.url})
+
+Some more text"""
+
+st.markdown(a)
+
+
+st.image(str(player_photo.url), width=400)
+st.image('https://cdn-images-1.medium.com/max/1024/1*u9U3YjxT9c9A1FIaDMonHw.png')
 
 
 @st.cache
