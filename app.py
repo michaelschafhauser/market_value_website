9b4aa020fe0e8ff7e302ae9cf1114203dd3fde1a
joao robarts <jpsrobarts@gmail.com>
2021-12-09 11:08:15 +0000

    picture added, formated overall element and player status dataframe

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd
 import requests
 import plotly.express as px
-from data import get_player_data, get_futdb_data
+from data import get_player_data, get_futdb_data, get_player_id, get_player_image
 import requests
 from dotenv import load_dotenv
 from requests.structures import CaseInsensitiveDict
 import os
 from dict_country_club import country_dict, club_dict
+from PIL import Image
 
 # HEADERS ==================================
 load_dotenv()
@@ -41,99 +42,101 @@ data = get_player_data(player)
 # futdb name search URL ==========================
 searched_player = get_futdb_data(player)
 
-"""
-# 🎉 Player estimated Value 🎉
-"""
-st.metric("Predicted value", data['prediction'])
+if data['prediction'][:3] == 'EUR' :
 
 
-# Player Stats DF
-list_of_stats = ['name', 'age', 'height', 'weight']
-stats_dictionary = {}
-for stat in list_of_stats:
-    stats_dictionary[stat] = str(searched_player[stat])
-
-# futdb image search URL =========================================
-URL3 = f"https://futdb.app/api/players/{searched_player['id']}/image"
-player_photo = requests.get(URL3)
-player_photo.url  #this line will provide a link to the photo
-
-st.metric("Player name", searched_player['name'])
-
-league_ref = searched_player["league"]
-nation_ref = searched_player["nation"]
-club_ref = searched_player["club"]
+    """
+    # 🎉 Player estimated Value 🎉
+    """
+    # player predicted value =======================
+    st.markdown(f"<h1 style='text-align: center;\
+                color: red;'>{data['prediction']} 💸</h1>",unsafe_allow_html=True)
 
 
-# NATIONS, LEAGUES, CLUBS ====================================
-player_geo_information = {}
-
-for i in range(len(country_dict)):
-    if nation_ref == country_dict[i]["id"]:
-        player_geo_information["Nationality"] = (country_dict[i]["name"])
-
-params_url4 = {'id': league_ref}
-league_resp = requests.get(URL4, params=params_url4, headers=headers).json()
-league_name = league_resp['items']
-for i in range(len(league_name)):
-    if league_ref == league_name[i]["id"]:
-        player_geo_information["League"] = (league_name[i]["name"])
+    stats_columns = st.columns(2)
+    # player image =================================
+    pic_path = get_player_image(get_player_id(player))
+    image = Image.open(pic_path)
+    stats_columns[0].image(image, use_column_width=False)
+    stats_columns[0].write(f"Overall Rating {data['features']['overall'][0]} ⭐")
+    # st.metric("Player name", searched_player['name'])
 
 
-for i in range(len(club_dict)):
-    if league_ref == club_dict[i]["league"] and club_ref == club_dict[i]["id"]:
-        player_geo_information["Club"] = (club_dict[i]["name"])
+    # Player Stats DF ============================================
+    list_of_stats = ['name', 'age', 'height', 'weight']
+    stats_dictionary = {}
+    for stat in list_of_stats:
+        stats_dictionary[stat] = str(searched_player[stat])
 
-# Player Stats DF 2
-df_geographic_loc = pd.DataFrame.from_dict(player_geo_information,
-                                           orient="index",
-                                           columns=['Player Stats'])
+    league_ref = searched_player["league"]
+    nation_ref = searched_player["nation"]
+    club_ref = searched_player["club"]
 
-# ================================================================
-
-
-@st.cache
-def get_player_stats():
-    return pd.DataFrame.from_dict(stats_dictionary, orient='index', columns=['Player Stats'])
+    for i in range(len(country_dict)):
+        if nation_ref == country_dict[i]["id"]:
+            stats_dictionary["Nationality"] = (country_dict[i]["name"])
 
 
-stats_df = get_player_stats()
-
-# Concated Player Stats DF
-frames = [stats_df, df_geographic_loc]
-df_concat = pd.concat(frames, sort=False)
-df_concat = df_concat.drop(labels=['name'], axis=0)
-df_concat = df_concat.rename({
-    "age": "Age",
-    "height": "Height (cm)",
-    "weight": "Weight (kg)"
-})
-df_concat
+    params_url4 = {'id': league_ref}
+    league_resp = requests.get(URL4, params=params_url4, headers=headers).json()
+    league_name = league_resp['items']
+    for i in range(len(league_name)):
+        if league_ref == league_name[i]["id"]:
+            stats_dictionary["League"] = (league_name[i]["name"])
 
 
-@st.cache
-def get_dataframe_data():
-    return pd.DataFrame.from_dict(data['features'])
+    for i in range(len(club_dict)):
+        if league_ref == club_dict[i]["league"] and club_ref == club_dict[i]["id"]:
+            stats_dictionary["Club"] = (club_dict[i]["name"])
 
 
-# RADAR CHART =======================================================
-def generate_radar_chart():
-    r=[]
-    theta = []
-    for key, value in data['features'].items():
-        r.append(value[0])
-        theta.append(key)
+    @st.cache
+    def get_player_stats():
+        return pd.DataFrame.from_dict(stats_dictionary, orient='index', columns=['Player Stats'])
 
-    df = pd.DataFrame(dict(r=r, theta=theta))
-    fig = px.line_polar(df, r='r', theta='theta', line_close=True)
-    return fig
 
-'''
-# Player Atributes
-'''
-df = get_dataframe_data()
-hdf = df.assign(hack='').set_index('hack')
-st.table(hdf)
+    stats_df = get_player_stats()
 
-radar = generate_radar_chart()
-st.write(radar)
+    stats_df = stats_df.rename({
+        "age": "Age",
+        "height": "Height (cm)",
+        "weight": "Weight (kg)"
+    })
+    stats_columns[1].table(stats_df)
+    # ====================================================================
+
+    @st.cache
+    def get_dataframe_data():
+        return pd.DataFrame.from_dict(data['features'])
+
+
+    # RADAR CHART =======================================================
+    def generate_radar_chart():
+        r=[]
+        theta = []
+        for key, value in data['features'].items():
+            if key != 'overall':
+                r.append(value[0])
+                theta.append(key)
+
+        df = pd.DataFrame(dict(r=r, theta=theta))
+        fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+        return fig
+
+    '''
+    # Player Attributes
+    '''
+    df = get_dataframe_data()
+    df.drop(columns=['overall'], inplace=True)
+    hdf = df.assign(hack='').set_index('hack')
+    st.table(hdf)
+
+    radar = generate_radar_chart()
+    st.write(radar)
+
+
+
+else:
+    """
+    # Uups 🤷🏼‍♂️
+    """
